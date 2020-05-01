56567b92e35cb6302b23c0cb13e0de7d33550e1d
Mejia-C <64386397+Mejia-C@users.noreply.github.com>
2020-04-30 23:16:45 -0500

    Create cpuinfo.sh

---
+++ cpuinfo.sh
@@ -0,0 +1,13 @@
+#! /bin/bash 
+echo "current User"
+hostname
+echo "CPU name and speed"
+lscpu | grep "MHz"
+lscpu | grep "Model name"
+echo "Free and Total Memory"
+vmstat -s | grep "total memory"
+echo "Free and Total swap space"
+vmstat -s | grep "total swap"
+vmstat -s | grep "free swap"
+echo "IP and subnet"
+ip -o -f inet addr show \ awk '/scope global/ {print $4}'
